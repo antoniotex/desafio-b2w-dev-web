f290c1f1e3a1bb970f29df149f87b4b0ef542aa3
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-27 09:26:28 -0300

    Adiciona novos comentários e novos produtos-teste

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,10 @@
-/**
- * Método para extrair somente números de uma string qualquer e converter para number
- */
 module.exports = {
+  /**
+  * Método para extrair somente números de uma string qualquer e converter para number
+  */
   regexOnlyNumbers: (id) => parseInt(id.replace(/\D+/g, '')),
+  /**
+   * Método para converter uma string em number
+   */
   convertStringToFloat: (string) => parseFloat(string)
 }                                                    
--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const getProduct = require('./lib')
 
-const base = {
-  product: 'https://www.americanas.com.br/produto/133718358/',
-}
-
-getProduct(base.product)
+module.exports = getProduct
 
