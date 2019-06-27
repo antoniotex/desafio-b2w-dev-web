fa85f554b34dd948a3cb6f688c14a76d73c2c1bb
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-27 00:48:41 -0300

    Refatora helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,7 @@
-module.exports = regexOnlyNumbers = (id) => {
-  return parseInt(id.replace(/\D+/g, ''))
-}
+/**
+ * Método para extrair somente números de uma string qualquer e converter para number
+ */
+module.exports = {
+  regexOnlyNumbers: (id) => parseInt(id.replace(/\D+/g, '')),
+  convertStringToFloat: (string) => parseFloat(string)
+}                                                    
