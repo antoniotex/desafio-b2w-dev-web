8a0be2bbe5b4a18f2e702d62d240e5747bfb82f9
Antonio Carlos <antoniotx.dev@gmail.com>
2019-06-28 01:52:32 -0300

    Adiciona testes nas funções de obter produto

--- test/get-product.test.js
+++ test/get-product.test.js
@@ -1,6 +1,6 @@
 const getProduct = require('../lib')
 
-const url = 'https://www.americanas.com.br/produto/133718358/'
+const urlMock = 'https://www.americanas.com.br/produto/133718358/'
 
 const mock = { 
   id: 133718358,
@@ -13,15 +13,53 @@ const mock = {
   price: 2199 
 }
 
-describe('getProduct function', () => {
-  let page = ''
+describe('Directory lib functions --->>', () => {
+  let pageProduct = ''
   test('function getProduct should return a string', async () => {
-    const data = await getProduct.getProduct(url)
-    page = data
+    const data = await getProduct.getProduct(urlMock)
+    pageProduct = data
     expect(typeof data).toBe('string')
   })
 
-  test('function _scraping should return object', () => {
-    expect(getProduct._scraping(page)).toEqual(mock)
+  test('_scraping should be a function / return an object / to equal mock', () => {
+    expect(typeof getProduct._scraping).toEqual('function')
+    expect(typeof getProduct._scraping(pageProduct)).toEqual('object')
+    expect(getProduct._scraping(pageProduct)).toEqual(mock)
+  })
+
+  test('_getProductId should be a function / return a number / to equal mock.id', () => {
+    expect(typeof getProduct._getProductId).toEqual('function')
+    expect(typeof getProduct._getProductId()).toBe('number')
+    expect(getProduct._getProductId()).toEqual(mock.id)
+  })
+
+  test('_getBreadcrumbs should be a function / return an array / to equal mock.breadcrumb', () => {
+    expect(typeof getProduct._getBreadcrumbs).toEqual('function')
+    expect(typeof getProduct._getBreadcrumbs()).toEqual('object')
+    expect(getProduct._getBreadcrumbs()).toEqual(mock.breadcrumb)
+  })
+
+  test('_getProductName should be a function / return a string / to equal mock.name', () => {
+    expect(typeof getProduct._getProductName).toEqual('function')
+    expect(typeof getProduct._getProductName()).toBe('string')
+    expect(getProduct._getProductName()).toEqual(mock.name)
+  })
+
+  test('_getProductImage should be a function / return a string / to equal mock.img', () => {
+    expect(typeof getProduct._getProductImage).toEqual('function')
+    expect(typeof getProduct._getProductImage()).toBe('string')
+    expect(getProduct._getProductImage()).toEqual(mock.img)
+  })
+
+  test('_getProductSeller should be a function / return a string / to equal mock.seller', () => {
+    expect(typeof getProduct._getProductSeller).toEqual('function')
+    expect(typeof getProduct._getProductSeller()).toBe('string')
+    expect(getProduct._getProductSeller()).toEqual(mock.seller)
+  })
+
+  test('_getProductPrice should be a function / return a number / to equal mock.price', () => {
+    expect(typeof getProduct._getProductPrice).toEqual('function')
+    expect(typeof getProduct._getProductPrice()).toBe('number')
+    expect(getProduct._getProductPrice()).toEqual(mock.price)
   })
 })
